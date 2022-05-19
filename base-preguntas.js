08be76da4c6055d747af9bcb0686964fed931e50
xtianbritos <xtiancuenta1@gmail.com>
2022-05-19 19:52:32 -0300

    Versión 1.0

---
+++ base-preguntas.js
@@ -0,0 +1,218 @@
+
+const base_preguntas ={
+
+    "ciencia": [
+        {
+            "categoría": "Ciencia",
+            "pregunta": "¿Qué desastre natural se mide con escala de Richter?",
+            "respuesta": "Terremotos",
+            "incorrecta1": "Tornados",
+            "incorrecta2": "Inundaciones",
+            "incorrecta3": "Huracanes",
+        },
+        {
+            "categoría": "Ciencia",
+            "pregunta": "¿Cuál es el planeta más cercano al sol?",
+            "respuesta": "Mercurio",
+            "incorrecta1": "Tierra",
+            "incorrecta2": "Marte",
+            "incorrecta3": "Júpiter",
+        },
+        {
+            "categoría": "Ciencia",
+            "pregunta": "¿De qué sustancia están hechas las uñas?",
+            "respuesta": "Queratina",
+            "incorrecta1": "Serotonina",
+            "incorrecta2": "Epidermis",
+            "incorrecta3": "Melanina",
+        },
+        {
+            "categoría": "Ciencia",
+            "pregunta": "¿Qué significa el ADN?",
+            "respuesta": "Ácido desoxirribonucleico",
+            "incorrecta1": "Ácido duoxirribonucleico",
+            "incorrecta2": "Ácido duoribonuclear",
+            "incorrecta3": "Ácido desoxirribonuclear",
+        },
+        {
+            "categoría": "Ciencia",
+            "pregunta": "¿”K” es el símbolo químico de qué elemento?",
+            "respuesta": "Potasio",
+            "incorrecta1": "Hidrógeno",
+            "incorrecta2": "Titanio",
+            "incorrecta3": "Cromo",
+        }
+    ],
+
+    "deportes" : [
+        {
+            "categoría": "Deportes",
+            "pregunta": "¿Cuál es el deporte nacional de Canadá?",
+            "respuesta": "Lacrosse",
+            "incorrecta1": "Baloncesto",
+            "incorrecta2": "Fútbol",
+            "incorrecta3": "Bolos",
+        },
+        {
+            "categoría": "Deportes",
+            "pregunta": "¿Qué boxeador fue conocido como “El más grande” y “El campeón del pueblo”?",
+            "respuesta": "Muhammad Ali",
+            "incorrecta1": "Mike Tyson",
+            "incorrecta2": "Floyd Mayweather",
+            "incorrecta3": "Oscar de la Hoya",
+        },
+        {
+            "categoría": "Deportes",
+            "pregunta": "¿Cuántos minutos dura un partido de rugby?",
+            "respuesta": "Ochenta minutos",
+            "incorrecta1": "Treinta minutos",
+            "incorrecta2": "Ciento veinte minutos",
+            "incorrecta3": "Sesenta minutos",
+        },
+        {
+            "categoría": "Deportes",
+            "pregunta": "¿En qué país se celebraron los primeros Juegos Olímpicos?",
+            "respuesta": "Grecia",
+            "incorrecta1": "Francia",
+            "incorrecta2": "japón",
+            "incorrecta3": "Italia",
+        },
+        {
+            "categoría": "Deportes",
+            "pregunta": "¿Cuál es el único país que ha jugado en todos los mundiales de fútbol?",
+            "respuesta": "Brasil",
+            "incorrecta1": "Inglaterra",
+            "incorrecta2": "Argentina",
+            "incorrecta3": "Italia",
+        }
+    ],
+
+    "geografia" : [
+        {
+            "categoría": "Geografía",
+            "pregunta": "¿Cuál es la capital de Australia?",
+            "respuesta": "Canberra",
+            "incorrecta1": "Adelaide",
+            "incorrecta2": "Sydney",
+            "incorrecta3": "Melbourne",
+        },
+        {
+            "categoría": "Geografía",
+            "pregunta": "¿Cuál es el país más propenso a terremotos del mundo?",
+            "respuesta": "Japón",
+            "incorrecta1": "Malasia",
+            "incorrecta2": "Corea",
+            "incorrecta3": "Taiwán",
+        },
+        {
+            "categoría": "Geografía",
+            "pregunta": "¿Cuántos países hay dentro del Reino Unido?",
+            "respuesta": "Cuatro",
+            "incorrecta1": "Tres",
+            "incorrecta2": "Cinco",
+            "incorrecta3": "Siete",
+        },
+        {
+            "categoría": "Geografía",
+            "pregunta": "¿Cuál es la isla más grande del mundo?",
+            "respuesta": "Groenlandia",
+            "incorrecta1": "Irlanda",
+            "incorrecta2": "Finlandia",
+            "incorrecta3": "Islandia",
+        },
+        {
+            "categoría": "Geografía",
+            "pregunta": "¿Dónde encontrarías la “Plaza de España”?",
+            "respuesta": "Italia",
+            "incorrecta1": "España",
+            "incorrecta2": "Brasil",
+            "incorrecta3": "Austria",
+        }
+    ],
+
+    "historia" : [
+        {
+            "categoría": "Historia",
+            "pregunta": "¿Cuántos alunizajes tripulados ha habido?",
+            "respuesta": "Seis",
+            "incorrecta1": "Ocho",
+            "incorrecta2": "Cinco",
+            "incorrecta3": "Siete",
+        },
+        {
+            "categoría": "Historia",
+            "pregunta": "¿En qué año se firmó la Declaración de Independencia de los Estados Unidos?",
+            "respuesta": "1776",
+            "incorrecta1": "1775",
+            "incorrecta2": "1678",
+            "incorrecta3": "1676",
+        },
+        {
+            "categoría": "Historia",
+            "pregunta": "¿Cuándo cayó el muro de Berlín?",
+            "respuesta": "1989",
+            "incorrecta1": "1988",
+            "incorrecta2": "1990",
+            "incorrecta3": "1991",
+        },
+        {
+            "categoría": "Historia",
+            "pregunta": "¿Qué padre fundador es conocido por su gran firma en la Declaración de Independencia de los Estados Unidos?",
+            "respuesta": "John Hancock",
+            "incorrecta1": "Benjamin Franklin",
+            "incorrecta2": "Alexander Hamilton",
+            "incorrecta3": "George Washington",
+        },
+        {
+            "categoría": "Historia",
+            "pregunta": "¿Cuánto duró la Guerra de los Cien Años?",
+            "respuesta": "116 años",
+            "incorrecta1": "110 años",
+            "incorrecta2": "100 años",
+            "incorrecta3": "99 años",
+        }
+    ],
+
+    "peliculas" : [
+        {
+            "categoría": "Películas",
+            "pregunta": "¿Cuál fue la primera película animada de largometraje que se lanzó?",
+            "respuesta": "Blanca Nieves y los siete enanitos",
+            "incorrecta1": "Pocahontas",
+            "incorrecta2": "La sirenita",
+            "incorrecta3": "Cenicienta",
+        },
+        {
+            "categoría": "Películas",
+            "pregunta": "En Matrix, ¿cuál es el color de la pastilla que toma Neo?",
+            "respuesta": "Roja",
+            "incorrecta1": "Amarilla",
+            "incorrecta2": "Verde",
+            "incorrecta3": "Azul",
+        },
+        {
+            "categoría": "Películas",
+            "pregunta": "¿Por qué película obtuvo Tom Hanks su primera nominación al Oscar?",
+            "respuesta": "Deseo cumplido",
+            "incorrecta1": "Forrest Gump",
+            "incorrecta2": "Apolo 13",
+            "incorrecta3": "Rescatando al soldado Ryan",
+        },
+        {
+            "categoría": "Películas",
+            "pregunta": "¿Quién interpretó al detective Rick Deckard en Blade Runner (1982)?",
+            "respuesta": "Harrison Ford",
+            "incorrecta1": "George Clooney",
+            "incorrecta2": "Richard Gere",
+            "incorrecta3": "Ryan Gosling",
+        },
+        {
+            "categoría": "Películas",
+            "pregunta": "Este aclamado cineasta ganador de un Oscar dirigió Happy Feet, Babe: el cerdito valiente y Mad Max: Furia en el camino.",
+            "respuesta": "George Miller",
+            "incorrecta1": "David Lynch",
+            "incorrecta2": "Christopher Nolan",
+            "incorrecta3": "Steven Spielberg",
+        }
+    ]
+}
---
+++ script.js
@@ -0,0 +1,199 @@
+
+let puntos = 0;
+let pregunta;
+let contador= 0;
+let opcionElegida;
+let ronda = 1;
+let categoriaElegida;
+let premio = 0;
+let Usuario ={};
+let miUsuario;
+
+
+// Chequeo si ya exista un registro de usuario en LocalStorage
+
+if (JSON.parse(localStorage.getItem("Usuario") == null)) {
+    // Si no existe creo el registro
+    Usuario.Puntaje = puntos;
+    localStorage.setItem('Usuario', JSON.stringify(Usuario));
+}
+
+
+// Función para ahorrar escribir document.getElementById(id) muchas veces
+function select_id(id) {
+    return document.getElementById(id);
+}
+
+
+// Escribo el puntaje actual del usuario
+select_id("puntaje").innerHTML= JSON.parse(localStorage.getItem("Usuario")).Puntaje;
+
+
+// Función que desordena la base de datos de preguntas y respuestas 
+function desordenarBase(){
+    base_preguntas.peliculas.sort(()=>Math.random()-0.5);
+    base_preguntas.historia.sort(()=>Math.random()-0.5);
+    base_preguntas.ciencia.sort(()=>Math.random()-0.5);
+    base_preguntas.geografia.sort(()=>Math.random()-0.5);
+    base_preguntas.deportes.sort(()=>Math.random()-0.5);
+    mostrarPreguntas(categoriaElegida, 0);
+}
+
+// Función que muestra solamente las preguntas y categorías en cada ronda
+function mostrarPreguntas(base, n) {
+    pregunta = base[n];
+    select_id("pregunta").innerHTML = pregunta.pregunta;
+    select_id("categoria").innerHTML = pregunta.categoría;
+    desordenarRespuestas(pregunta);
+}
+
+
+// Función que desordena aleatóriamente las preguntas en cada ronda
+function desordenarRespuestas(pregunta){
+    let posibles_respuestas = [pregunta.respuesta, pregunta.incorrecta1, pregunta.incorrecta2, pregunta.incorrecta3, pregunta.incorrecta4];
+    posibles_respuestas.sort(()=>Math.random()-0.5);
+
+    select_id("opcion1").innerHTML = posibles_respuestas[0];
+    select_id("opcion2").innerHTML = posibles_respuestas[1];
+    select_id("opcion3").innerHTML = posibles_respuestas[2];
+    select_id("opcion4").innerHTML = posibles_respuestas[3];
+}
+
+
+// Función que reemplaza la selección e categorías por el juego en sí, y establece el premio de la ronda 1
+function iniciarJuego(){
+    premio = 10;
+    select_id("container").innerHTML = `<div class="container_juego">
+        <div class="encabezado">
+            <div class="categoria" id="categoria">Categoría</div>
+            <div class="pregunta" id="pregunta">Pregunta</div>
+        </div>
+        
+        <div class="opcion" id="opcion1">Opción 1</div>
+        <div class="opcion" id="opcion2">Opción 2</div>
+        <div class="opcion" id="opcion3">Opción 3</div>
+        <div class="opcion" id="opcion4">Opción 4</div>
+        <h3>Premio: <span id="premio"></span> puntos</h3>
+        <a href="index.html" class="boton rojo">Abandonar partida</a>
+    `;
+
+    // Si se hace click para abandonar la partida
+    document.querySelector(".rojo").addEventListener("click", ()=> {
+        // Actualizo el puntaje sumándole los premios ganados hasta el momento
+        miUsuario = JSON.parse(localStorage.getItem("Usuario"));
+        miUsuario.Puntaje += puntos;
+        localStorage.setItem('Usuario', JSON.stringify(miUsuario));
+        // Se reinicia el valor del premio
+        premio = 0;
+    });
+}
+
+
+// Función que se ejecuta si la respuesta es incorrecta, mostrando un mensaje
+function respIncorrecta(){
+    select_id("header").innerHTML = `<div class="mensaje">
+            <h1 id="error">¡Respuesta incorrecta!</h1>
+            <h2>Tu puntaje es: <span id="puntaje"></span></h2>
+            <a href="index.html" class="boton">Volver a intentarlo</a>
+        </div>
+    `;
+    // Muestro el puntaje sin cambio ya que se perdió lo acumulado en las rodnas previas
+    select_id("puntaje").innerHTML = JSON.parse(localStorage.getItem("Usuario")).Puntaje;
+    // Oculto el juego
+    select_id("container").innerHTML = "";
+}
+
+
+//Funcion que se ejecuta cuando se responde todas las preguntas correctamente 
+function ganarJuego(){
+    
+    // Actualizo el puntaje
+    miUsuario = JSON.parse(localStorage.getItem("Usuario"));
+    miUsuario.Puntaje += puntos;
+    localStorage.setItem('Usuario', JSON.stringify(miUsuario));  
+    
+    // Muestro el mensaje de felicitaciones y el puntaje
+    select_id("header").innerHTML = `<div class="mensaje">
+            <h1 id="correcto">¡Felicitaciones!</h1>
+            <h2>Tu puntaje es: <span id="puntaje"></span></h2>
+            <a href="index.html" class="boton">Elegír categoría</a>
+        </div>
+    `;
+
+    select_id("puntaje").innerHTML= miUsuario.Puntaje;
+    select_id("container").innerHTML = "";
+    puntos = 0;
+}
+
+
+// Función que realiza todo lo necesario para iniciar el juego una vez se elije una categoría
+function catSeleccionada(){
+    iniciarJuego();        
+    desordenarBase();
+    select_id("ronda").innerHTML = ronda;
+    select_id("premio").innerHTML = premio;
+}
+
+// -----------------------------------------Escuchadores de eventos------------------------------------------------
+
+
+// Cuando se hace click a una categoría se inicia el juego
+document.addEventListener("click", (e)=>{
+
+    if (e.composedPath()[1].id == "peliculas" || e.composedPath()[0].id == "peliculas"){
+        categoriaElegida = base_preguntas.peliculas;
+        catSeleccionada();
+    }
+    if (e.composedPath()[1].id == "geografia" || e.composedPath()[0].id == "geografia"){
+        categoriaElegida = base_preguntas.geografia;
+        catSeleccionada();
+    }
+    if (e.composedPath()[1].id == "historia" || e.composedPath()[0].id == "historia"){
+        categoriaElegida = base_preguntas.historia;
+        catSeleccionada();
+    }
+    if (e.composedPath()[1].id == "ciencia" || e.composedPath()[0].id == "ciencia"){
+        categoriaElegida = base_preguntas.ciencia;
+        catSeleccionada();
+    }
+    if (e.composedPath()[1].id == "deportes" || e.composedPath()[0].id == "deportes"){
+        categoriaElegida = base_preguntas.deportes;
+        catSeleccionada();
+    }
+})
+
+
+// Cuando se hace click a una respuesta
+document.addEventListener("click", (e)=>{
+    if (e.target.classList == "opcion"){
+
+        opcionElegida = e.target.innerHTML;
+
+        // Si la respuesta es correcta se aumenta la ronda, se suma el premio al acomulado y se aumenta el premio 
+        if (opcionElegida == categoriaElegida[contador].respuesta) {
+            contador += 1;
+            ronda += 1;
+            puntos += premio;
+            premio += 10;
+            select_id("ronda").innerHTML = ronda;
+            select_id("premio").innerHTML = premio;
+
+            // Si se han contestado menos de 5 preguntas se muestra la siguiente
+            if (contador<5){
+                mostrarPreguntas(categoriaElegida, contador);
+            }
+            // Sino se muestra el mensaje de felicitaciones y se resetean el contador y la ronda
+            else {
+                ganarJuego();
+                contador = 0;
+                ronda = 1;
+            }
+        }
+        // Si la respuesta es incorrecta se muestra el mensaje de error y se reinicia el valor del premio
+        else {
+            respIncorrecta();
+            premio = 0;
+        }
+    }
+
+})
